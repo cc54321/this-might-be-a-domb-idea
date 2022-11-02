1e8d91fa3324a11bfd71e042aed4a53894365906
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-11-02 12:53:58 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+let para = document.querySelector('p');
+
+para.style.color ="lightblue";
+
+let head = document.querySelector('h1');
+
+head.style.fontSize = "48px";
+
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let item13 = document.querySelector("#item-13");
+
+item13.style.opacity = "0.5";
+
+let item3 = document.querySelector("#item-3");
+
+item3.innerText = "I say, Hi!";
+
+
+
+img2.style.height = '300px';
+
+let item16 = document.querySelector("#item-16");
+item16.className = "item";
+item16.innerText = "Won't get fooled again";
